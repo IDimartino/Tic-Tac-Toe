a32ed20b9373ed346695cc28e9c827fe3d607f95
IDimartino <i.dimartino1408@gmail.com>
2026-01-09 15:37:47 +0100

    chore: initial project setup with Hilt, Compose, and Testing dependencies

---
+++ app/src/main/java/com/app/tictactoe/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.app.tictactoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.app.tictactoe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TicTacToeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/app/tictactoe/TicTacToeApp.kt
@@ -0,0 +1,7 @@
+package com.app.tictactoe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TicTacToeApp : Application()
